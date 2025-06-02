e71a0a40e7e42c311b600fd06dce167a55c6cb33
hoozsed <hoozsed@epitech.eu>
2025-06-02 22:42:30 +0400

    fix

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <unistd.h>
+#include<string.h>
 
-int main(int ac, char **av)
+int my_strlen(char *sent)
 {
     int count = 0;
-    int idx = 0;
+
+    for (int i = 0; sent[i] != '\0'; i++) {
+        count++;
+    }
+    return count;
+}
+int main(int ac, char **av)
+{
+    int start = 0;
+    int end = my_strlen(av[1]) - 1;
 
     if (ac != 2) {
-        write(2, "not enought", 11);
+        write(2, "not enough", 11);
+        return 84;
     }
-    while (av[1][idx] == ' ') {
-        count++;
-        idx++;
+    while (av[1][start] == ' ' && av[1][start] != '\0') {
+        start++;
     }
-    printf("%d\n", count);
+    while (av[1][end] == ' ' && end >= start) {
+        end--;
+    }
+    for (int i = start; i <= end; i++)
+        write(1, &av[1][i], 1); 
     return 0;
 }
+
 
 
 
